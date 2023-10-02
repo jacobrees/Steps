a8c7d01a45b4d91ddcd3a810745919c091e2421d
jacobrees <jacob.rees97@yahoo.com>
2023-10-02 20:23:24 +0100

    Complete styling the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-function App () {
-  return <div>Hello React</div>
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
